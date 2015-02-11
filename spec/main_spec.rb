5e81170aab6f672e38f150b5bdc66ac422cfb679
Evan Sobkowicz <evansobkowicz@gmail.com>
2015-02-11 16:12:45 -0500

    initial app

---
+++ spec/main_spec.rb
@@ -0,0 +1,9 @@
+describe "Application 'table-switch-test'" do
+  before do
+    @app = UIApplication.sharedApplication
+  end
+
+  it "is a ProMotion app" do
+    @app.delegate.should.be.kind_of(PM::Delegate)
+  end
+end
